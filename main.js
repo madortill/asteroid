d902ebf80af69a3038522f9bed489159c7c9a0f9
madortill <mador.till@gmail.com>
2022-06-13 18:15:39 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ var nPart = 0;
 
 var matrix = [
     // part 0
-    [
-        {
+    [{
             // opening game question- page 1
             divName: ["opening"],
-            functions: [ `pop_buttons(document.querySelector(".odot-logo"), "0", "1")`,]
+            functions: [`pop_buttons(document.querySelector(".odot-logo"), "0", "1")`, ]
         },
         {
             // about- page 1
@@ -16,8 +15,7 @@ var matrix = [
         }
     ],
     // part 1
-    [
-        {
+    [{
             // instructions- page 0
             divName: ["instructions"], // the last div contains the speech bubble
             functions: [`pop_buttons(document.querySelector(".start-button"), "+0", "+1")`] // array of functions that are needed to the page. If the functions contain the word "pop", it will happen only once and will be popped out of the array afterwards
@@ -39,8 +37,10 @@ var matrix = [
 const coordinate_1 = "000";
 const coordinate_2 = "111";
 
-var dragX = 0, dragY = 0;
-var w = window.innerWidth, h = window.innerHeight;
+var dragX = 0,
+    dragY = 0;
+var w = window.innerWidth,
+    h = window.innerHeight;
 var angle = 0;
 
 window.addEventListener('load', () => {
@@ -53,7 +53,7 @@ let cooardinates = 0;
 // check if the user can start the mission
 checkCode = (event) => {
     let input = event.target;
-    
+
     // if the code is correct
     if (input.value === eval(`coordinate_${input.getAttribute("data-num")}`)) {
         input.removeEventListener('input', checkCode);
@@ -64,7 +64,7 @@ checkCode = (event) => {
     // if the user assigned all coordinates
     if (cooardinates === document.querySelectorAll('input').length) {
         // move part
-        movePage("1","0"); 
+        movePage("1", "0");
         document.querySelector(".odot-logo").style.display = "none";
     }
 }
@@ -90,7 +90,7 @@ movePage = (part, num) => {
 function hidePage() {
     // hides last divs
     for (let i = 0; i < matrix[nPart][nPage].divName.length; i++) {
-        document.querySelector("#" + matrix[nPart][nPage].divName[i]).style.display =  "none";
+        document.querySelector("#" + matrix[nPart][nPage].divName[i]).style.display = "none";
     }
 }
 
@@ -99,7 +99,7 @@ function showPage() {
     for (let i = 0; i < matrix[nPart][nPage].divName.length; i++) {
         document.querySelector("#" + matrix[nPart][nPage].divName[i]).style.display = "block";
     }
-    callPageFunctions();    
+    callPageFunctions();
 }
 
 callPageFunctions = (event) => {
@@ -108,22 +108,22 @@ callPageFunctions = (event) => {
     if (matrix[nPart][nPage].functions.length > 0) {
         let nFunction = 0;
         while (nFunction < matrix[nPart][nPage].functions.length) {
-          eval(matrix[nPart][nPage].functions[nFunction]);
-          // functions that contains the word "pop" will accur only once
-          if (matrix[nPart][nPage].functions[nFunction].includes("pop")) {
-            matrix[nPart][nPage].functions.splice(nFunction , 1);
-            // since the function happens only once there is no need in adding nFunction +1
-          } else {
-            nFunction++;
-          }
+            eval(matrix[nPart][nPage].functions[nFunction]);
+            // functions that contains the word "pop" will accur only once
+            if (matrix[nPart][nPage].functions[nFunction].includes("pop")) {
+                matrix[nPart][nPage].functions.splice(nFunction, 1);
+                // since the function happens only once there is no need in adding nFunction +1
+            } else {
+                nFunction++;
+            }
         }
     }
 }
 
 // function that adds events listeners to buttons that affects the page's display- called only one time for each button
 pop_buttons = (button, part, num) => {
-    button.addEventListener('click', function() {
-        movePage(part, num)   
+    button.addEventListener('click', function () {
+        movePage(part, num)
     });
 }
 
@@ -137,10 +137,13 @@ let shotFunc;
 // let shotW = 0;
 
 activeShot = () => {
-    shotW = 1 * Math.sign(angle);
-    shotH = Math.abs(((h - dragY)/(dragX - 0.5 * w))) * Math.abs(shotW);
-    document.querySelector(".lazer").style.left = `${0.5 * w}px`;
-    document.querySelector(".lazer").style.bottom = `0px`;
+    shotW = Math.sign(angle);
+    if (-1 < angle && angle < 1) {
+        shotH = 3;
+    } else {
+        shotH = Math.abs(((h - dragY) / (dragX - 0.5 * w))) * Math.abs(shotW);
+    }
+    document.querySelector("#lazer").style.bottom = `0px`;
     shotFunc = setInterval(shotAnimation, 10 * (1 / Math.abs(angle)));
 
     document.querySelector("#mission .container").removeEventListener("mousemove", rotateShot);
@@ -153,31 +156,33 @@ rotateShot = (e) => {
     dragY = e.pageY;
     angle = Math.atan((dragX - 0.5 * w) / (h - dragY)) * 180 / Math.PI;
     console.log(angle);
-    document.querySelector(".lazer").style.transform = `rotate(${angle}deg)`;
+    document.querySelector("#lazer").style.transform = `rotate(${angle}deg)`;
+    document.querySelector(".launcher").style.transform = `rotate(${angle}deg)`;
 }
 
 var shotRect;
 var asteroidRect;
-let shotsCounter = 0; 
+let shotsCounter = 0;
 shotAnimation = () => {
     // shotW++;
     // shotH = Math.abs(((h - dragY)/(dragX - 0.5 * w))) * shotW;
-    document.querySelector(".lazer").style.bottom = `${parseFloat(document.querySelector(".lazer").style.bottom) + shotH}px`;
-    document.querySelector(".lazer").style.left = `${parseFloat(document.querySelector(".lazer").style.left) + shotW}px`;
+    document.querySelector("#lazer").style.bottom = `${parseFloat(window.getComputedStyle(document.querySelector("#lazer"), null).getPropertyValue("bottom")) + shotH}px`;
+    document.querySelector("#lazer").style.left = `${parseFloat(window.getComputedStyle(document.querySelector("#lazer"), null).getPropertyValue("left")) + shotW}px`;
 
-    shotRect = document.querySelector(".lazer").getBoundingClientRect();
+    shotRect = document.querySelector("#lazer").getBoundingClientRect();
     asteroidRect = document.querySelector("#asteroid").getBoundingClientRect();
     if ((asteroidRect.y <= shotRect.y && shotRect.y <= asteroidRect.bottom && asteroidRect.x <= shotRect.x && shotRect.x <= asteroidRect.right)) {
         clearInterval(shotFunc);
         // send to winning function of explosion!
         win();
-    } else if ((parseFloat(document.querySelector(".lazer").style.left) > w) || (parseFloat(document.querySelector(".lazer").style.left) < 0)) {
-            // user surpassed height
-        if ((parseFloat(document.querySelector(".lazer").style.bottom) > h)) {
-            shotW = 0;
+        // user surpassed height or width
+    } else if ((parseFloat(window.getComputedStyle(document.querySelector("#lazer"), null).getPropertyValue("bottom")) > h) || (parseFloat(window.getComputedStyle(document.querySelector("#lazer"), null).getPropertyValue("left")) > w) || (parseFloat(window.getComputedStyle(document.querySelector("#lazer"), null).getPropertyValue("left")) < 0)) {
+        if ((parseFloat(window.getComputedStyle(document.querySelector("#lazer"), null).getPropertyValue("bottom")) > h)) {
+            shotW = 0; 
             angle = 0;
             clearInterval(shotFunc);
-            document.querySelector(".lazer").style.cssText = "transform: rotate(0deg); left: 50%; bottom: 0vh;";
+            document.querySelector("#lazer").style.cssText = `transform: rotate(0deg); left: 48.5vw; bottom: 0vh;`;
+            document.querySelector(".launcher").style.cssText = `transform: rotate(0deg);`;
             document.querySelector("#mission button").disabled = false;
             pop_eventListener(document.querySelector("#mission .container"), 'mousemove', rotateShot);
             nMultipleCorrectAnswers--;
@@ -189,10 +194,10 @@ shotAnimation = () => {
             // user reached wall - change direction
         } else {
             angle = angle * -1;
-            document.querySelector(".lazer").style.transform = `rotate(${angle}deg)`;
+            document.querySelector("#lazer").style.transform = `rotate(${angle}deg)`;
             shotW = shotW * -1;
         }
-    } 
+    }
 }
 
 retry = () => {
@@ -202,10 +207,20 @@ retry = () => {
 }
 
 win = () => {
-    movePage("1", "0");
-    document.querySelector('#instructions .instructions').innerHTML = "איזה מזל! הצלחתם לפוצץ את האסטרואיד!";
-    document.querySelector(`.start-button`).style.display = "none";
+    document.querySelector(`#asteroid`).style.animationPlayState = "paused";
+    document.querySelector(`#asteroid`).style.backgroundImage = "url(assets/media/explosion.gif)";
+    setTimeout(function() {
+        document.querySelector(`#asteroid`).style.display = "none";
+        setTimeout(function() {
+            movePage("1", "0");
+            document.querySelector('#instructions .instructions').innerHTML = "איזה מזל! הצלחתם לפוצץ את האסטרואיד!";
+            document.querySelector(`.start-button`).style.display = "none";
+        }, 1500);
+    }, 700);
 }
+
+
+
 
 
 // let currLock;
@@ -236,7 +251,7 @@ win = () => {
 //     }
 //     // document.querySelectorAll('.lock').forEach((lock) => {
 //     //     lock.addEventListener('click', question);
-        
+
 //     // });
 // }
 
@@ -284,10 +299,3 @@ win = () => {
 //         document.querySelector('#mission .instructions').innerHTML = "כל הכבוד! חשפתם את המספר הבא!"
 //     }
 // }
-
-
-
-
-
-
-
